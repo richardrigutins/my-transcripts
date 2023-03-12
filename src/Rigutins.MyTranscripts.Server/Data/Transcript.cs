9100a003de7ce1bf2dd0a921c1c5ee8bf0a598c7
richardrigutins <47950599+richardrigutins@users.noreply.github.com>
2023-03-12 15:16:07 +0100

    Show transcriptions in progress

---
+++ src/Rigutins.MyTranscripts.Server/Data/Transcript.cs
@@ -0,0 +1,10 @@
+﻿namespace Rigutins.MyTranscripts.Server.Data;
+
+public class Transcript
+{
+	public string Id { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string? OneDriveUrl { get; set; }
+	public bool IsInProgress { get; set; } = false;
+	public DateTimeOffset? CreatedDateTime { get; set; }
+}
--- src/Rigutins.MyTranscripts.Server/SpeechRecognition/SpeechRecognitionState.cs
+++ src/Rigutins.MyTranscripts.Server/SpeechRecognition/SpeechRecognitionState.cs
@@ -1,7 +1,10 @@
-﻿namespace Rigutins.MyTranscripts.Server.SpeechRecognition;
+﻿using Rigutins.MyTranscripts.Server.Data;
+
+namespace Rigutins.MyTranscripts.Server.SpeechRecognition;
 
 public class SpeechRecognitionState
 {
 	public bool IsRecognizing { get; set; } = false;
+	public Transcript? TranscriptInProgress { get; set; }
 	public List<string> RecognizedSentences { get; set; } = new();
 }
